d78f044c2ea698e7b5805a99411fa19569e73188
Bruno da Luz <brunolabx@gmail.com>
2016-12-12 22:35:51 -0200

    add validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+
+  validates :full_name, :location, :password, :presence => true
+  validates :password, :confirmation => true
+  validates :bio, :presence => true, :minimum => 30, :allow_blank => false
+  validates :email, :presence => true,
+                    :format => { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ },
+                    :uniqueness => true
+
 end
