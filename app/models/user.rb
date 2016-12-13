1ce4a68e7546b187ff7b7ce8961bdc1709042ec4
Bruno da Luz <brunolabx@gmail.com>
2016-12-13 01:06:48 -0200

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
 
-  validates :full_name, :location, :password, :presence => true
-  validates :password, :confirmation => true
-  validates :bio, :presence => true, :allow_blank => false, length: { :minimum => 30 }
-  validates :email, :presence => true,
-                    :format => { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ },
-                    :uniqueness => true
+  attr_accessor :bio, :email, :full_name, :location, :password
+
+  validates :full_name, :location, :password, presence: true
+  validates :password, confirmation: true
+  validates :bio, presence: true, allow_blank: false, length: { minimum: 30 }
+  validates :email, presence: true,
+                    format: { with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ },
+                    uniqueness: true
 
 end
