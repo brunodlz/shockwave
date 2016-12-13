a2ec4a4902674d985ed67cc00a9c017e54187be6
Bruno da Luz <brunolabx@gmail.com>
2016-12-13 01:29:31 -0200

    add update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,18 @@ class UsersController < ApplicationController
 
   end
 
+  def update
+    respond_to do |format|
+      if @user.update_attributes(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def set_user
