da0187742e9a8159dfe647c091721a8ba6046168
Bruno da Luz <brunolabx@gmail.com>
2016-12-14 00:43:32 -0200

    migrate
    modified:   db/schema.rb
    
    new file:   db/migrate/20161214013922_rename_password_on_users.rb
    modified:   db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161213000144) do
+ActiveRecord::Schema.define(version: 20161214013922) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 20161213000144) do
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
     t.string   "email"
-    t.string   "password"
+    t.string   "password_digest"
     t.string   "location"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
